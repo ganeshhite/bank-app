64f9179c9bc0a7563612e072ba0ddee6a93da045
Ganesh <gabeshhite33@gmail.com>
2026-03-24 08:57:36 +0000

    record 999

--- app-once.sh
+++ app-once.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysql -u root -pCorpau@123 bank_db -e "INSERT INTO transactions (amount) VALUES (100);"
+mysql -u root -pCorpau@123 bank_db -e "INSERT INTO transactions (amount) VALUES (999);"
 
